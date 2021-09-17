edea51d974103472cb19c413ab4091e87a25f860
Matvey <42606931+Metoredom@users.noreply.github.com>
2021-09-17 21:01:16 +0300

    Sofya

--- main.cpp
+++ main.cpp
@@ -1,53 +1,55 @@
-/******************************
- * Matvei Barbashov, mb21162
- * A32. Create a programm to find the smallest common divisible 
- * of two given natural numbers.
- * Programm created: 16.09.2021
- * ****************************/
+/*
+ Yulpatova Sofya, sy21002
+ A24. Given two natural numbers m and n.
+ Print sum of last n digits in number m. Splitting into digits should be carried out numerically.
+ The date of creation is 16.09.2021
+*/
+
+
 
 #include <iostream>
 
 using namespace std;
 
-int main()
-{
-    int n, m; //Values for the first and second numbers
-    int ok;   //Key to loop programm
-
-    //Looping programm
-    do
-    {
-        cout << "Input the first number" << endl;
-        cin >> n;
-        cout << "Input the second number" << endl;
+int main() {
+    int ok;
+    do {
+        // ABOUT m: Value contains integer m - number
+        // ABOUT n: Value contains integer n - count of digits, starting with the last, for sum numbers m
+        int m, n;
+        cout << "Enter the integer m: ";
         cin >> m;
+        cout << "Enter the variable n - the number of the last digits m for which you need to calculate the amount: ";
+        cin >> n;
+        int fake_m = m; // the value - fake of m to count the length os number m
+        int counter_m = 0; // the value-counter for number m
+        int sum = 0; // # Value contains integer - the amount of last n numbers of m
 
-        //Checking that numbers are nonzero, since zero is a not natural number
-        if (n > 0 && m > 0)
-        {
-            int gcd = 1;     //Value for the greatest common divisor
-            int lcm = n * m; //Value for the lowest common multiple
-
-            //Euclid's algorithm for finding GCD
-            while (m != 0)
-            {
-                int c = m;
-                m = n % m;
-                n = c;
+        if (n <= 0 or m <= 0) { // checking the n is less than zero + WARNING because the count is a POSITIVE number
+            cout << "WARNING, numbers must be positive" << endl;
+        } else {
+            // checking the length of m
+            while (abs(fake_m) > 0) {
+                counter_m++; // updating counter of digits
+                fake_m /= 10; // discarding the last digit of fake_m
             }
-            gcd = n;
+            if (n > counter_m) { // checking that length of n is more than m + WARNING because the number n can't be more than the length of m
+                cout << "WARNING, the length of number m is less than n (number of digits)" << endl;
+            } else {
 
-            lcm /= gcd; //LCM of A and B numbers is A*B/GCD
-
-            cout << "Lowest common multiple is " << lcm << endl;
+                for (; n > 0; n--) { // the loop to count last n digits
+                    sum += m % 10; // the counting sum of last digit of number m in looping
+                    m /= 10; // discarding the last digit of the number m
+                }
+                cout << "the amount of last n numbers of m is: " << sum << endl;
+            }
         }
-        else
-            cout << "Error: Entered values aren't natural numbers" << endl;
 
-        cout << "Continue (1) or quit (0)?" << endl;
+
+        cout << " Continue (1) or quit (0)?" << endl;
         cin >> ok;
+    }
+    while (ok == 1);
 
-    } while (ok == 1);
 
-    return 0;
-}
+} 
